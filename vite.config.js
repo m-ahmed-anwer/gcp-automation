71746e09f38254fce21e5edeb84f0880830a16b8
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-10-21 05:40:45 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Configure the preview server to use the PORT environment variable
+  preview: {
+    port: 8080,
+    host: true,
+  },
+});
